e9d9d891e27ed1092c51eb942a8204676e196101
Nikita Karpuk <bitxon.dev@gmail.com>
2023-10-02 23:19:16 +0200

    NO Spring

--- src/main/java/bitxon/app/Application.java
+++ src/main/java/bitxon/app/Application.java
@@ -1,28 +1,51 @@
 package bitxon.app;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
-@RestController
-@SpringBootApplication
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 public class Application {
+    static final int PORT = 8080;
+    static final String APP_NAME = Optional.ofNullable(System.getenv("APP_NAME")).orElse("unknown");
 
-    @Value("${APP_NAME:unknown}")
-    private String appName;
+    public static void main(String[] args) throws IOException {
+        LogManager.getLogManager()
+            .readConfiguration(Application.class.getResourceAsStream("/logging.properties"));
+        var log = Logger.getLogger(Application.class.getName());
 
-    @GetMapping("/hello")
-    public String hello() {
-        log.info("GET /hello -> {}", appName);
-        return appName;
+        var server = HttpServer.create(new InetSocketAddress(PORT), 0);
+        server.createContext("/", new AllRequestsHandler());
+        server.start();
+        log.info("Started '%s'".formatted(APP_NAME));
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
 
+    static class AllRequestsHandler implements HttpHandler {
+        Logger log = Logger.getLogger(AllRequestsHandler.class.getName());
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String method = exchange.getRequestMethod();
+            String path = exchange.getRequestURI().getPath();
+            String body = exchange.getRequestBody().available() > 0
+                ? new String(exchange.getRequestBody().readAllBytes(), StandardCharsets.UTF_8)
+                : null;
+
+            log.info("'%s' '%s' '%s' -> %s".formatted(method, path, body, APP_NAME));
+
+            exchange.sendResponseHeaders(200, APP_NAME.getBytes().length);
+            var output = exchange.getResponseBody();
+            output.write(APP_NAME.getBytes());
+            output.flush();
+            exchange.close();
+        }
+    }
 }
